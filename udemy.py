e49d7d3f8fbeb55a95ceeceacdd9e9db131838fc
Juracy Filho <juracy.filho@dataprev.gov.br>
2017-08-11 16:00:18 -0300

    Initial version

---
+++ udemy.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import requests
+
+PAGE_SIZE = 200
+
+
+class Aggregate(object):
+    def __init__(self):
+        self.students = 0
+        self.revenues = 0
+
+    def __add__(self, value):
+        self.students += 1
+        self.revenues += value
+        return self
+
+    def __str__(self):
+        return '%2d students $%6.2f' % (self.students, self.revenues)
+
+
+class Udemy(object):
+    def __init__(self):
+        self.token = os.getenv('UDEMY_TOKEN')
+        if not self.token:
+            raise Exception('Missing token, please set environment variable: UDEMY_TOKEN')
+
+        self.statement = os.getenv('UDEMY_STATEMENT')
+        if not self.statement:
+            raise Exception('Missing statement, please set environment variable: UDEMY_STATEMENT')
+
+        self.headers = {
+            'authorization': 'Bearer ' + self.token,
+            'x-udemy-authorization': 'Bearer ' + self.token,
+        }
+
+    def summarize(self):
+        self.totals = {}
+        for sale in self.sales['data']:
+            self.totals[sale['formatted_date']] = \
+                self.totals.get(sale['formatted_date'], Aggregate()) + \
+                sale['instructor_share']
+
+        self.totals_refunds = sum(
+            refund['instructor_refund_amount'] for refund in self.refunds['data'])
+
+    def get_refunds(self):
+        url = ('https://www.udemy.com/api-2.0/statements/{}/' +
+               'refunds?page=1&page_size={}').format(self.statement, PAGE_SIZE)
+        response = requests.get(url, headers=self.headers)
+        self.refunds = response.json()
+
+    def get_sales(self):
+        url = ('https://www.udemy.com/api-2.0/statements/{}/' +
+               'sales?page=1&page_size={}').format(self.statement, PAGE_SIZE)
+        response = requests.get(url, headers=self.headers)
+        self.sales = response.json()
+
+    def display(self):
+        from datetime import datetime
+
+        print('Date: ', datetime.now())
+        for date in sorted(self.totals.keys()):
+            print('{}: {}'.format(date.split()[1], self.totals[date]))
+
+        sums = sum(t.revenues for t in self.totals.values())
+        students = sum(t.students for t in self.totals.values())
+        print()
+        print('total: {} students ${:7.2f}'.format(students, sums))
+        print('refunds:', '%6.2f' % self.totals_refunds)
+        print('revenues:', '%6.2f' % (sums - self.totals_refunds))
+
+
+if __name__ == '__main__':
+    try:
+        u = Udemy()
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+    u.get_sales()
+    u.get_refunds()
+    u.summarize()
+    u.display()
